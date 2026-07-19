93832ce00802dd39a06e78796e3ef97dcfd259c7
JLKnowles <j.lucious.knowles@gmail.com>
2026-07-19 17:03:10 -0600

    Add player movement

--- main.py
+++ main.py
@@ -17,6 +17,7 @@ def main():
       if event.type == pygame.QUIT:
        return
      dt = clock.tick(60) / 1000
+     player.update(dt)
      screen.fill("Black")
      player.draw(screen)
      pygame.display.flip()
