ae8622acdbe5f3b7ce02eba04623a070304b0825
JLKnowles <j.lucious.knowles@gmail.com>
2026-08-01 13:58:13 -0600

    Add collision detection and Add shooting mechanics

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 import pygame
+import sys
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
-from logger import log_state
+from logger import log_state, log_event
 from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
+from shot import Shot
+
 def main():
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
     print(f"Screen width: {SCREEN_WIDTH}")
@@ -9,9 +14,15 @@ def main():
     pygame.init()
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+    shots = pygame.sprite.Group()
     Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    Shot.containers = (shots, updatable, drawable)
+    AsteroidField.containers = (updatable, )
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    asteroidfield = AsteroidField()
     clock = pygame.time.Clock()
     dt = 0.0
     while True:
@@ -21,6 +32,11 @@ def main():
        return
      dt = clock.tick(60) / 1000
      updatable.update(dt)
+     for asteroid in asteroids:
+       if player.collides_with(asteroid):
+         log_event("player_hit")
+         print("Game over!")
+         sys.exit()
      screen.fill("Black")
      for drawables in drawable:
       drawables.draw(screen)
